056685341b997611c913c27661db065bae631923
albertevieites <albertevieites@gmail.com>
2022-10-02 00:24:23 +0100

    feat: :construction: style rsvp

--- src/pages/rsvp.jsx
+++ src/pages/rsvp.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { StaticImage } from "gatsby-plugin-image";
 
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 const Rsvp = () => {
   return (
-    <div>
+    <div className="rsvp">
       <Navbar />
       <form
         id="form"
@@ -13,20 +14,20 @@ const Rsvp = () => {
         action="https://api.web3forms.com/submit"
         method="POST"
       >
-        <h3 class="contact__title">Contact me</h3>
-
+        <h3>Please fill one per person</h3>
         <input
           type="hidden"
           name="access_key"
           value="8120b10c-514e-48af-88ed-f7830d8d79f4"
         />
 
-        <div class="contact__data">
+        <div className="rsvp__name">
           <label for="name">
-          <input type="text" name="name" placeholder="Full Name" required />
+          <input type="text" name="name" placeholder="Your Name" required />
           </label>
         </div>
 
+        <h3>Please select one</h3>
         <fieldset>
           <label>
             <input type="radio" name="radio" value="yes" /> I'm coming
@@ -36,8 +37,9 @@ const Rsvp = () => {
           </label>
         </fieldset>
 
-        <div class="contact__button">
-          <input type="submit" value="Send it" />
+        <div class="rsvp__button">
+          <StaticImage src="../images/icons/finger.png" alt="finger icon" />
+          <input type="submit" value="Let me know!" />
         </div>
       </form>
       <Footer />
